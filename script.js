42f9d991268a425bbb05ec21b331f6b9b2487719
victor-VN <v1ctoroliveiranasc@gmail.com>
2022-11-27 13:00:08 -0300

    added new image for milan and infos for best scorer and assistants

--- script.js
+++ script.js
@@ -27,26 +27,27 @@ function loadCard(event){
     `<div class="card card-style" style="width: 22rem;">
     <img src="${info.winnerImage}" height="${info.bigImageHeight}" width="${info.bigImageWidth}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title text-center">SS LAZIO</h5>
+      <h5 class="card-title text-center">${info.winner}</h5>
     </div>
     <ul class="list-group">
       <li class="list-group-item table-card"> 
         <span style="color: #ab85db;">Vice Campeão:</span> 
-        Manchester United
-        <td class="text-center"><img src="./assets/img/escudos-small/Manchester_United_FC_crest.svg" alt="" height="30" width="30"></td>
+        ${info.vice}
+        <td class="text-center"><img src="${info.viceImage}" alt="" height="30" width="30"></td>
       </li>
       <li class="list-group-item table-card"> 
         <span style="color: #ab85db;">Artilheiro:</span> 
-        N'Doye
-        <td class="text-center"><img src="./assets/img/escudos-small/FC_Copenhagen_logo.svg" alt="" height="30" width="30"></td>
+        ${info.bestScorer}
+        <td class="text-center"><img src="${info.bestScorerClubImage}" alt="" height="30" width="30"></td>
       </li>
       <li class="list-group-item table-card">  
         <span style="color: #ab85db;">Assistências:</span> 
-        N/A
+        ${info.bestAssistant}
+        <td class="text-center"><img src="${info.bestAssistantClubImage}" alt="" height="30" width="30"></td>
       </li>
       <li class="list-group-item table-card"> 
         <span style="color: #ab85db;">Treinador:</span> 
-        Eduardo da Silva
+        ${info.coach}
       </li>
     </ul>
   </div>`
@@ -76,10 +77,10 @@ function getData(){
                 viceImage: "./assets/img/escudos-small/Chelsea_FC.svg",
                 viceTableImageHeight: 30,
                 viceTableImageWidth: 30,
-                bestScorer: "N'Doye",
-                bestScorerClubImage: "./assets/img/escudos-small/FC_Copenhagen_logo.svg",
-                bestAssistant: "N/A",
-                bestAssistantClubImage: "",
+                bestScorer: "Lukaku",
+                bestScorerClubImage: "./assets/img/escudos-small/Chelsea_FC.svg",
+                bestAssistant: "Verrati",
+                bestAssistantClubImage: "./assets/img/escudos-small/Paris_Saint-Germain_F.C..svg",
             },
             {
                 edition: 4,
@@ -94,28 +95,28 @@ function getData(){
                 viceImage: "./assets/img/escudos-small/Borussia_Dortmund_logo.svg",
                 viceTableImageHeight: 30,
                 viceTableImageWidth: 30,
-                bestScorer: "N'Doye",
-                bestScorerClubImage: "./assets/img/escudos-small/FC_Copenhagen_logo.svg",
+                bestScorer: "Arjen Robben",
+                bestScorerClubImage: "./assets/img/escudos-small/Logo_FC_Bayern_München_(2002–2017).svg",
                 bestAssistant: "N/A",
-                bestAssistantClubImage: ""
+                bestAssistantClubImage: "./assets/img/placeholderna.jpg"
             },
             {
                 edition: 3,
                 winner: "Milan",
                 vice: "Manchester United",
                 coach: "Victor Nascimento",
-                winnerImage: "./assets/img/escudos-small/Logo_of_AC_Milan.svg",
+                winnerImage: "./assets/img/escudos-small/ac-milan.svg",
                 tableImageHeight: 30,
                 tableImageWidth: 30,
                 bigImageHeight: 200,
-                bigImageWidth: 200,
+                bigImageWidth: 100,
                 viceImage: "./assets/img/escudos-small/Manchester_United_FC_crest.svg",
                 viceTableImageHeight: 30,
                 viceTableImageWidth: 30,
-                bestScorer: "N'Doye",
-                bestScorerClubImage: "./assets/img/escudos-small/FC_Copenhagen_logo.svg",
+                bestScorer: "N/A",
+                bestScorerClubImage: "./assets/img/placeholderna.jpg",
                 bestAssistant: "N/A",
-                bestAssistantClubImage: ""
+                bestAssistantClubImage: "./assets/img/placeholderna.jpg"
             },
             {
                 edition: 2,
@@ -130,10 +131,10 @@ function getData(){
                 viceImage: "./assets/img/escudos-small/Logo_FC_Bayern_München_(2002–2017).svg",
                 viceTableImageHeight: 30,
                 viceTableImageWidth: 30,
-                bestScorer: "N'Doye",
-                bestScorerClubImage: "./assets/img/escudos-small/FC_Copenhagen_logo.svg",
+                bestScorer: "N/A",
+                bestScorerClubImage: "./assets/img/placeholderna.jpg",
                 bestAssistant: "N/A",
-                bestAssistantClubImage: ""
+                bestAssistantClubImage: "./assets/img/placeholderna.jpg"
             },
             {
                 edition: 1,
@@ -151,7 +152,7 @@ function getData(){
                 bestScorer: "N'Doye",
                 bestScorerClubImage: "./assets/img/escudos-small/FC_Copenhagen_logo.svg",
                 bestAssistant: "N/A",
-                bestAssistantClubImage: ""
+                bestAssistantClubImage: "./assets/img/placeholderna.jpg"
             }
         ]
     }
